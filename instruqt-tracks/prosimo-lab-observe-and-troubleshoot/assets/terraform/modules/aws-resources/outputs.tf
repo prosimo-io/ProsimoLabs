cf5bb18cdaa0d07a684ed7d79aa64f2e1ff7d530
Nathan Pearce <nathan.pearce@gmail.com>
2023-10-10 11:12:54 -0700

    Improve tf output readability

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/modules/aws-resources/outputs.tf
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/modules/aws-resources/outputs.tf
@@ -2,6 +2,7 @@
 output "aws_ec2_public_ip" {
   value = aws_instance.ec2_linux.public_ip
 }
+
 output "aws_eip_public_ip" {
   value = aws_eip.eip.public_ip
 }
@@ -11,5 +12,5 @@ output "aws_vpc_id" {
 }
 
 output "ssh_access" {
-  value = "ssh -i '~/prosimo-lab/assets/terraform/${var.aws_ec2_key_pair_name}.pem' ec2-user@${aws_eip.eip.public_ip}"
+  value = "${var.aws_ec2_name} - ${aws_instance.ec2_linux.private_ip} => ssh -i '~/prosimo-lab/assets/terraform/${var.aws_ec2_key_pair_name}.pem' ec2-user@${aws_eip.eip.public_ip}"
 }
