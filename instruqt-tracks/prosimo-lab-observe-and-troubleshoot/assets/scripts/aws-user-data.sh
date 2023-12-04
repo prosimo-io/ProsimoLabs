72c2496ad66bd5a898b17449e1b1c34c9ae0e2a2
Nathan Pearce <nathan.pearce@gmail.com>
2023-12-04 12:32:44 -0800

    Update iperf script

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/scripts/aws-user-data.sh
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/scripts/aws-user-data.sh
@@ -8,6 +8,31 @@ echo “Hello Prosimo MCN fans and Welcome” > /var/www/html/index.html
 
 sudo yum -y install iperf3
 
+
+# Create Traffic generator script for each port.
+
+%{ for port in upstream_ports ~}
+sudo bash -c 'cat <<"EOT" > /home/ec2-user/iperf-client-${port}.sh
+#!/bin/bash
+
+while true; do
+
+    # Run for default of 10 seconds
+    /usr/bin/iperf3 -c ${upstream_host} -p ${port} -t 30 --logfile /home/ec2-user/iperf-client-${port}.log --verbose
+
+    # Sleep for 35 seconds
+    sleep 35
+
+done
+EOT'
+%{ endfor ~}
+
+sudo chmod +x /home/ec2-user/iperf-client-${port}.sh
+sudo chown ec2-user:ec2-user /home/ec2-user/iperf-client-${port}.sh
+
+
+# Create Systemd Unit for Traffic generator script, for each port.
+
 %{ for port in upstream_ports ~}
 sudo bash -c 'cat <<"EOT" > /etc/systemd/system/iperf-client-${port}.service
 [Unit]
@@ -20,7 +45,7 @@ Restart=on-failure
 RestartSec=1
 RemainAfterExit=yes
 User=ec2-user
-ExecStart=/usr/bin/iperf3 -c ${upstream_host} -p ${port} -t 14400 --logfile /home/ec2-user/iperf-client-${port}.log --verbose
+ExecStart=/home/ec2-user/iperf-client-${port}.sh
 
 [Install]
 WantedBy=multi-user.target
