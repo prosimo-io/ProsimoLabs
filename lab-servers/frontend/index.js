15cfb02bc030c40032c74d03c444eb24989342b1
Nathan Pearce <nathan.pearce@gmail.com>
2023-12-18 20:43:44 -0800

    Add Frontend, Message Routers, and Middleware servers

--- lab-servers/frontend/index.js
+++ lab-servers/frontend/index.js
@@ -5,35 +5,59 @@ const UPSTREAM_API_ADDR = process.env.UPSTREAM_API_ADDR || 'localhost'
 const UPSTREAM_API_PORT = process.env.UPSTREAM_API_PORT || 3000
 const UPSRTEAM_API = `${UPSTREAM_API_ADDR}:${UPSTREAM_API_PORT}`
 
-const http = require('http')
 const express = require('express')
 const app = module.exports = express()
 
 // For parsing application/json
 app.use(express.json())
-
-app.get('/', (req, resp) => {
-  console.log(`${new Date().toString()} - ${req.method} - ${req.path}`)
-  resp.send('Front End: Hello World')
-})
+// For serving static content
+app.use(express.static('public'))
 
 app.get('/api', (req, resp) => {
   console.log(`${new Date().toString()} - ${req.method} - ${req.path}`)
   fetchData(req.path)
   .then(api_resp => {
-    console.log(`api_resp: ${JSON.stringify(api_resp)}`)
+    console.log(`Message Router: /api: ${JSON.stringify(api_resp)}`)
     resp.send(api_resp)
   })
 })
 
+// example: http://localhost:3000/api/users/
+app.get('/api/users', (req, resp) => {
+  console.log(`${new Date().toString()} - ${req.method} - ${req.path}`)
+  fetchData(req.path)
+  .then(api_resp => {
+    console.log(`Message Router: /api/users: ${JSON.stringify(api_resp)}`)
+    resp.send(api_resp)
+  })
+})
+
+// example: http://localhost:3000/api/user/tobi/repos/
+app.get('/api/user/:name/repos', (req, resp) => {
+  console.log(`${new Date().toString()} - ${req.method} - ${req.path}`)
+  fetchData(req.path)
+  .then(api_resp => {
+    console.log(`Message Router: /api/user/:name/repos: ${JSON.stringify(api_resp)}`)
+    resp.send(api_resp)
+  })
+})
+
+
 async function fetchData(path) {
   try {
-      const response = await fetch('http://'+UPSRTEAM_API+path);
-      const data = await response.json();
-      console.log(data);
+    const response = await fetch('http://'+UPSRTEAM_API+path);
+    console.log(`response codes: ${response.status}`)
+    if (!response.ok) {
+      return JSON.parse(`{"Status": "Error response from API", "code": "${response.status}"}`)
+    }
+    else {
+      const data = await response.json()
+      console.log(data)
       return data
+    }
   } catch (error) {
-      console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error)
+    return JSON.parse(`{"Status": "Error reaching API", "Message": "Something went really wrong. Blame the network!"}`)
   }
 }
 
--- lab-servers/middleware/index.js
+++ lab-servers/middleware/index.js
@@ -16,11 +16,44 @@ app.get('/api', (req, res) => {
   res.send(status)
 })
 
-app.put('/api', (req, res) => {
-  console.log(`${new Date().toString()} - ${req.method} - ${req.path}`)
-  res.send(req.body)
+
+app.get('/api/users', (req, res) => {
+  res.send(users);
+});
+
+app.get('/api/repos', (req, res) => {
+  res.send(users);
+});
+
+// example: http://localhost:3000/api/user/tobi/repos/
+app.get('/api/user/:name/repos', (req, res, next) => {
+  const name = req.params.name
+  const user = userRepos[name]
+  if (user) res.send(user)
+  else next()
 })
 
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 })
+
+
+// these three objects will serve as our faux database
+
+const repos = [
+  { name: 'express', url: 'https://github.com/expressjs/express' },
+  { name: 'stylus', url: 'https://github.com/learnboost/stylus' },
+  { name: 'cluster', url: 'https://github.com/learnboost/cluster' }
+]
+
+const users = [
+  { name: 'tobi' }
+  , { name: 'loki' }
+  , { name: 'jane' }
+]
+
+const userRepos = {
+  tobi: [repos[0], repos[1]]
+  , loki: [repos[1]]
+  , jane: [repos[2]]
+}
