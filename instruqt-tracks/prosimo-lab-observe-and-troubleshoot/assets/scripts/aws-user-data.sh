29874cf4c7c4c4786f78f9b40006b512f92c6e43
Nathan Pearce <nathan.pearce@gmail.com>
2023-12-22 15:57:30 -0800

    add Reverse proxy

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/scripts/aws-user-data.sh
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/scripts/aws-user-data.sh
@@ -3,6 +3,27 @@ sudo yum update -y && ## << Fixes 'Yum lock error'
 sudo touch /home/ec2-user/USERDATA_EXECUTED
 sudo yum install -y git
 
+sudo yum install -y httpd.x86_64
+
+TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
+PUBLIC_ADDR=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --header "X-aws-ec2-metadata-token: $TOKEN")
+
+cat <<EOT > /etc/httpd/conf.d/prosimo-lab.conf 
+<VirtualHost *:80>
+#      ServerName $${PUBLIC_ADDR}
+#      ServerAlias www.$${PUBLIC_ADDR}
+#      ServerAdmin webmaster@$${PUBLIC_ADDR}
+ 
+      ProxyPreserveHost Off
+      ProxyPass / http://localhost:3000/
+      ProxyPassReverse / http://localhost:3000/
+ 
+      ErrorLog /var/log/httpd/lab-error.log
+      CustomLog /var/log/httpd/access.log combined
+</VirtualHost>
+EOT
+
+
 # Install NodeJS 20
 sudo yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 sudo yum install nsolid -y
@@ -10,6 +31,7 @@ sudo yum install nsolid -y
 # Fetch NodesJS App
 mkdir /home/ec2-user/prosimo-lab/
 sudo chown ec2-user:ec2-user /home/ec2-user/prosimo-lab/
+cd /home/ec2-user/
 git clone https://github.com/prosimo-io/ProsimoLabs.git
 ## REMOVE
 cd /home/ec2-user/ProsimoLabs
@@ -30,7 +52,7 @@ After=network.target
 [Service]
 Environment="UPSTREAM_API_ADDR=${upstream_host}"
 Environment="UPSTREAM_API_PORT=${upstream_port}"
-Environment="PORT=80"
+Environment="PORT=3000"
 Type=simple
 Restart=on-failure
 RestartSec=1
@@ -42,4 +64,5 @@ ExecStart=/usr/bin/node .
 WantedBy=multi-user.target
 EOT'
 
-sudo systemctl start lab-message-router.service
+sudo systemctl start lab-message-router
+sudo systemctl start httpd
