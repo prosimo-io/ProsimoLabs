a2cb182358f9256bac745e65b13a1aac5e4dda9c
Nathan Pearce <nathan.pearce@gmail.com>
2024-01-21 16:04:43 -0800

    tidy

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/prosimo_infra/onboard_accounts/main.tf
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/prosimo_infra/onboard_accounts/main.tf
@@ -1,19 +1,3 @@
-# initiate required Providers
-terraform {
-  required_providers {
-    prosimo = {
-      source  = "prosimo-io/prosimo"
-      version = "~> 3.10.0"
-    }
-  }
-}
-
-provider "prosimo" {
-  token    = var.prosimo_token
-  insecure = true
-  base_url = "https://${var.prosimo_team_name}.admin.prosimo.io"
-}
-
 # Onboard CSP Account into Prosimo Dashboard
 
 resource "prosimo_cloud_creds" "aws" {
