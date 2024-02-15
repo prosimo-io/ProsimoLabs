142b9e291515b913d7fa44ddc170b6aaebf768fa
Nathan Pearce <nathan.pearce@gmail.com>
2024-02-15 09:51:37 -0800

    output update

--- resources/terraform/lab_resources/aws_eu_west_1/outputs.tf
+++ resources/terraform/lab_resources/aws_eu_west_1/outputs.tf
@@ -5,7 +5,7 @@ output "ssh_access_aws_eu_west_1" {
 }
 
 output "aws_ec2_public_ip1" {
-  value = module.aws_eu_west_1_vpc1.aws_ec2_public_ip
+  value = module.aws_eu_west_1_vpc1.aws_eip_public_ip
 }
 
 output "ssh_access_aws_eu_west_2" {
@@ -13,7 +13,7 @@ output "ssh_access_aws_eu_west_2" {
 }
 
 output "aws_ec2_public_ip2" {
-  value = module.aws_eu_west_1_vpc2.aws_ec2_public_ip
+  value = module.aws_eu_west_1_vpc2.aws_eip_public_ip
 }
 
 output "vpc1_id" {
