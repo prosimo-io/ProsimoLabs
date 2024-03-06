08a84e8a6c261067cae59c2d38cdc8cfa6111df4
Nathan Pearce <nathan.pearce@gmail.com>
2024-03-05 23:00:43 -0800

    add Api Fw outputs

---
+++ resources/terraform/lab_resources/azure_security_pools/outputs.tf
@@ -0,0 +1,17 @@
+## Output variable definitions
+
+output "ssh_access_azure_api_fw_eu" {
+  value = module.azure_northeurope_api_fw_pool.ssh_access
+}
+
+output "azure_vm_api_fw_eu_public_ip" {
+  value = module.azure_northeurope_api_fw_pool.azure_vm_public_ip
+}
+
+output "azure_northeurope_api_fw_pool_vnet_id" {
+  value = module.azure_northeurope_api_fw_pool.azure_vnet_id
+}
+
+output "api_fw_eu_public_subnets" {
+  value = module.azure_northeurope_api_fw_pool.public_subnets[0]
+}
