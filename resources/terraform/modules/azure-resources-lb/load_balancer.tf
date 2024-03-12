b1e770c4f96de902cdf964bf92b07e381565fed9
Nathan Pearce <nathan.pearce@gmail.com>
2024-03-11 20:05:02 -0700

    Add rule, probe, and public IP to LB

--- resources/terraform/modules/azure-resources-lb/load_balancer.tf
+++ resources/terraform/modules/azure-resources-lb/load_balancer.tf
@@ -1,11 +1,19 @@
+resource "azurerm_public_ip" "api_fw_lb" {
+  name                = "${var.prefix}-publicip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_lb" "api_fw_lb" {
   name                = "TestLoadBalancer"
   location            = var.api_fw_pool.azure_location
   resource_group_name = module.azure_northeurope_api_fw_pool.azure_rg_name
   frontend_ip_configuration {
-    name               = "frontend"
-    private_ip_address = "10.20.1.20"
-    subnet_id          = module.azure_northeurope_api_fw_pool.azure_subnet_id
+    name                 = "frontend"
+    private_ip_address   = "10.20.1.20"
+    public_ip_address_id = azurerm_public_ip.api_fw_lb.id
+    subnet_id            = module.azure_northeurope_api_fw_pool.azure_subnet_id
   }
 }
 
@@ -19,4 +27,22 @@ resource "azurerm_lb_backend_address_pool_address" "api_fw" {
   backend_address_pool_id = azurerm_lb_backend_address_pool.api_fw.id
   virtual_network_id      = module.azure_northeurope_api_fw_pool.azure_vnet_id
   ip_address              = "10.20.1.100"
+}
+
+resource "azurerm_lb_probe" "api_fw_lb" {
+  name            = "probe"
+  loadbalancer_id = azurerm_lb.api_fw_lb.id
+  protocol        = "Tcp"
+  port            = 8080
+}
+
+resource "azurerm_lb_rule" "example" {
+  name                           = "http-lb-rule"
+  loadbalancer_id                = azurerm_lb.api_fw_lb.id
+  probe_id                       = azurerm_lb_probe.api_fw_lb.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.api_fw.id]
+  frontend_ip_configuration_name = "API_FW_LB_Frontend"
+  protocol                       = "Tcp"
+  frontend_port                  = "80"
+  backend_port                   = "8080"
 }
