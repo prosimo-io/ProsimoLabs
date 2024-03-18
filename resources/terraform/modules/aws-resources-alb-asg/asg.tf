e70c5c804be5fb873cb93864f8dbc8e36551846d
Nathan Pearce <nathan.pearce@gmail.com>
2024-03-18 08:40:55 -0700

    typo

--- resources/terraform/modules/aws-resources-alb-asg/asg.tf
+++ resources/terraform/modules/aws-resources-alb-asg/asg.tf
@@ -66,5 +66,5 @@ resource "aws_autoscaling_group" "application_asg" {
 
 resource "aws_autoscaling_attachment" "application_asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.application_asg.name
-  lb_target_group_arn    = aws_alb_target_group.alb_tg.arn
+  lb_target_group_arn    = aws_lb_target_group.alb_tg.arn
 }
